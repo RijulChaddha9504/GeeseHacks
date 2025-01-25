b1d1f11c462e2e008a6cc9992faf3e2b43ce1c78
Evan He <evan.he24@gmail.com>
2025-01-25 14:26:00 -0500

    created navbar and hero, imported shadcn

---
+++ geesehacks2025-nextjs/app/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link"
+
+const Navbar = () => {
+  return (
+    <nav className="bg-black p-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-white text-2xl font-bold">
+          GeeseTalk
+        </Link>
+        <div className="space-x-4">
+          <Link href="" className="text-white">
+            Home
+          </Link>
+          <Link href="" className="text-white">
+            Learn
+          </Link>
+          <Link href="" className="text-white">
+            Profile
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
+
---
+++ geesehacks2025-nextjs/app/pages/Hero.jsx
@@ -0,0 +1,20 @@
+const Hero = () => {
+  return (
+    <div className="text-center bg-gray-950 w-full h-screen">
+
+      <div className="h-[50vh] w-full flex flex-col justify-center items-center">
+        <h1 className="text-9xl font-bold mb-4 text-white">GeeseTalk</h1>
+        <p className="text-xl mb-8 text-white">Improve your speaking skills with Mr. Goose</p>
+        <a
+          href="/learn"
+          className="bg-gray-700 text-white px-6 py-3 rounded-lg text-lg"
+        >
+          Start Learning
+        </a>
+      </div>
+
+    </div>
+  )
+}
+
+export default Hero
