25c11435e4cf6b220842ba00ca95a79e4fc63f09
Ethan Chen <ethansunchen@gmail.com>
2025-01-26 01:23:34 -0500

    Basic graph on profile

--- geesehacks2025-nextjs/app/profile/page.js
+++ geesehacks2025-nextjs/app/profile/page.js
@@ -4,55 +4,41 @@ import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "@/com
 import { getSession, getUserById } from "../db/queries"
 import Image from "next/image"
 import { Progress } from "@/components/ui/progress"
+import { redirect } from "next/navigation"
+import Chart from "./chart"
+
 
 const ProfilePage = async () => {
     const session = await getSession();
+
+    if(!session){
+        redirect("/api/auth/signin");
+    }
+
     const user = (session) ? await getUserById(session?.user.id) : null;
 
-    return (
-        <div className="h-auto bg-gray-100 min-h-screen">
-            <div className="bg-gradient-to-br from-gray-800 to-gray-950 pt-20 flex flex-col items-center justify-center h-[calc(100vh-20rem)] text-center">
-                <Image alt={user.name + "'s profile picture"} src={user.image} width={196} height={196} className="mb-4 rounded-3xl"/>
-                <h1 className="text-5xl font-extrabold mb-8 text-white tracking-tight">
-                {user.name + "'s Profile"}
-                </h1>
-            </div>
-            <div className="mt-8 px-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-center">Placeholder</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <Progress value={40}/>
-                    </CardContent>
-                </Card>
-        
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-center">Placeholder</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <Progress value={40}/>
-                    </CardContent>
-                </Card>
-        
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-center">Placeholder</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <Progress value={40}/>
-                    </CardContent>
-                </Card>
+    const name = user ? user.name : "";
+    const img = user ? user.image : "";
+    const pastTotalPerformances = user.pastTotalPerformances;
 
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="text-center">Placeholder</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <Progress value={40}/>
-                    </CardContent>
-                </Card>
+    return (
+
+        <div className="h-auto bg-gray-100 min-h-screen">
+            <div className="bg-gradient-to-br from-gray-800 to-gray-950 pt-20 flex flex-col items-center justify-center min-h-screen text-center">
+                <Image alt={name + "'s profile picture"} src={img} width={196} height={196} className="mb-4 rounded-3xl"/>
+                <h1 className="text-5xl font-extrabold mb-8 text-white tracking-tight">
+                {name + "'s Profile"}
+                </h1>
+                <div className="mt-12">
+                    <Card>
+                        <CardHeader>
+                            <CardTitle className="text-center">Past Performance</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <Chart pastTotalPerformances={pastTotalPerformances} />
+                        </CardContent>
+                    </Card>
+                </div>
             </div>
         </div>
     )
